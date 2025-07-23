17a49e58a703e6a4ba4d447547ad659c0341c836
Ruan Henry <caruaru288@gmail.com>
2025-07-23 10:54:21 -0300

    Criação das funcionalidades

--- Banco.java
+++ Banco.java
@@ -1,3 +1,15 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Banco {
-    
+    private List<Conta> listaDeContas;
+
+    public Banco() {
+        this.listaDeContas = new ArrayList<>();
+    }
+
+    void criarConta(String nome) {
+        
+    }
 }
--- Conta.java
+++ Conta.java
@@ -1,3 +1,15 @@
 public class Conta {
+    private String nome;
+    private int numeroConta;
+    private double saldo;
+    private int contador;
+
+    public Conta(String nome, int conta) {
+        this.contador += 1;
+        this.nome = nome;
+        this.numeroConta = contador;
+        this.saldo = 0;
+    }
     
+   
 }
